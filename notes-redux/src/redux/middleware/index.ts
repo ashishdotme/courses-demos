fcf81aa0c4a3b7af2e0be8b41bb9302fc15c3b9e
Ashish Patel <ashishsushilpatel@gmail.com>
2020-08-20 05:48:05 +0530

    chore: restructure folders

--- notes-redux/src/redux/middleware/index.ts
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware } from 'redux';
-import loggingMiddleware from './logging';
-import localStorageMiddleware from './local-storage';
-
-
-export default applyMiddleware(
-    loggingMiddleware,
-    localStorageMiddleware,
-);
